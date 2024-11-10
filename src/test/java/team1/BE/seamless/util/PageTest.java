07c63b7cc3ff381e986c9bb1da173cf9906e82ff
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-11-10 21:29:02 +0900

    test: page관련 테스트코드 작성

---
+++ src/test/java/team1/BE/seamless/util/PageTest.java
@@ -0,0 +1,40 @@
+package team1.BE.seamless.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import team1.BE.seamless.util.page.PageMapper;
+import team1.BE.seamless.util.page.PageResult;
+import team1.BE.seamless.util.page.SingleResult;
+
+class PageTest {
+
+    @Test
+    void singleResult(){
+//        given
+        String[] test = "test".split("");
+
+//        when
+        SingleResult<String[]> singleResult = new SingleResult<>(test);
+
+//        then
+        assertEquals(test, singleResult.getResultData());
+    }
+
+    @Test
+    void page의_데이터는_페이지여야_한다(){
+//        given
+        String test = "test";
+
+//        when
+        Page<String> page = new PageImpl<>(Arrays.stream(test.split("")).toList());
+
+
+//        then
+        assertEquals(test, String.join("",page.stream().toList()));
+
+    }
+}
