b0c10003df3650c1e3f392888971e8de4fdb6bb0
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-11-13 01:10:25 +0900

    feat:토큰 검증 예외처리 구현

--- src/main/java/team1/be/seamless/util/fiter/TokenAuthenticationFilter.java
+++ src/main/java/team1/be/seamless/util/fiter/TokenAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package team1.be.seamless.util.fiter;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,11 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import team1.be.seamless.util.auth.JwtToken;
+import team1.be.seamless.util.errorException.CustomExceptionHandler;
+import team1.be.seamless.util.errorException.RuntimeHandler;
+import team1.be.seamless.util.errorException.StatusResponse;
+import team1.be.seamless.util.page.SingleResult;
 
 @Component
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
@@ -28,17 +34,24 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
-
-        String path = request.getRequestURI();
-        String method = request.getMethod();
-
+        response.setCharacterEncoding("utf-8");
+//        String path = request.getRequestURI();
+//        String method = request.getMethod();
+//
         String token = request.getHeader(AUTHORIZATION_HEADER);
         if (token != null && token.startsWith(BEARER_PREFIX)) {
             token = token.substring(7);
-            jwtToken.validateToken(token).getExpiration().after(new Date());
-            setAuthentication(token);
-        }
+            try{
+                jwtToken.validateToken(token);
+                setAuthentication(token);
+            } catch (RuntimeHandler e){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 설정
+                response.setContentType("application/json");
+                response.getWriter().write("{\"errorCode\": 401, \"errorMessage\": \"" + e.getMessage() + "\"}");
 
+                return;
+            }
+        }
         filterChain.doFilter(request, response);
     }
 
